f354d704c738c1bc462eb3fe4099843975785ce7
ahmed saber <ahmed.saber@alahlymomkn.com>
2024-08-16 16:21:16 +0300

    Routes | update project routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';  
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },  
+  { path: 'login', component: LoginComponent },  
+  { path: 'products', component: ProductsComponent, canActivate: [AuthGuard] } 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
